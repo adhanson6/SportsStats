72a968e478d66297629569d5e2c32dde3e0b60dc
Alex <adhanson6@gmail.com>
2019-09-11 20:41:50 -0500

    Initial Project Structure

---
+++ src/main/java/com/ahansondev/sportsstats/SportsstatsApplication.java
@@ -0,0 +1,13 @@
+package com.ahansondev.sportsstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsstatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsstatsApplication.class, args);
+	}
+
+}
